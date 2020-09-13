ea505b3432dd1349689eb0d9e904e8f1bbee3097
Ashish Chahal <ashish.chahal27@gmail.com>
2020-09-13 18:47:36 +0530

    large row styling

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -4,7 +4,7 @@ import "../Styles/Row.css";
 
 const baseUrl = `https://image.tmdb.org/t/p/original/`;
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   // A snippet of code which runs based on a specific condition/variable
@@ -33,8 +33,10 @@ function Row({ title, fetchUrl }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
-            className="row__poster"
-            src={`${baseUrl}${movie.poster_path}`}
+            className={`row__poster ${isLargeRow && `row__posterLarge`}`}
+            src={`${baseUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
