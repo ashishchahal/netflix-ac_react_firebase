479f6c3895e03c0b648cb6d9f081ec4c4978b288
Ashish Chahal <ashish.chahal27@gmail.com>
2020-09-13 18:10:35 +0530

    implemnting apis, fetching movies data in rows

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Row from './Components/Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1 className="app__heading">NETFLIX 🚀</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
