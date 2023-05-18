e642a710ab3c5edf61c53d4ae42a0e82a5d4b691
Ashish Chahal <ashish.chahal27@gmail.com>
2023-05-19 01:23:04 +0530

    adding webpack to project

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+// webpack.config.js
+
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// module.exports = ({ mode } = { mode: "production" }) => {
+//   console.log(`mode is: ${mode}`);
+
+//   return {
+//     mode,
+//     entry: "./src/index.js",
+//     output: {
+//       publicPath: "/",
+//       path: path.resolve(__dirname, "build"),
+//       filename: "bundled.js",
+//     },
+//     plugins: [
+//       new HtmlWebpackPlugin({
+//         template: "./public/index.html",
+//       }),
+//     ],
+//   };
+// };
+
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    chunkFilename: "[id].js",
+    publicPath: "",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[name]__[local]___[hash:base64:5]",
+              },
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [["autoprefixer", {}]],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: "url-loader?limit=10000&name=img/[name].[ext]",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+};
